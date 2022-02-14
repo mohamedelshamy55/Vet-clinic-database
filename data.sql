be58ecf5a725ef47abaed4c00bf44bc5501dca21
mohamedelshamy55 <mohamedelshamy55@gmail.com>
2022-02-14 15:08:47 +0200

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,13 +1,13 @@
 /* Populate database with sample data. */
 
 INSERT INTO animals (animal_name, date_of_birth, escape_attempts, neutered, weight_per_kg)
-VALUES ('Agumon',  '02/02/2020', '0', 'true', '10.23');
+VALUES ('Agumon',  '2020/02/03', '0', 'true', '10.23');
 
 INSERT INTO animals (animal_name, date_of_birth, escape_attempts, neutered, weight_per_kg)
 VALUES ('Gabumon', '2018/11/15', '2', 'true', '8');
 
 INSERT INTO animals (animal_name, date_of_birth, escape_attempts, neutered, weight_per_kg)
-VALUES ('Pikachu', '2021/01/17', '1', 'false', '15.04');
+VALUES ('Pikachu', '2021/01/07', '1', 'false', '15.04');
 
 INSERT INTO animals (animal_name, date_of_birth, escape_attempts, neutered, weight_per_kg)
-VALUES ('Devimon', '2017/04/12', '5', 'true', '11');
+VALUES ('Devimon', '2017/05/12', '5', 'true', '11');
