a771435ec47f252db1cce88f5d8b07ede7678012
mohamedelshamy55 <mohamedelshamy55@gmail.com>
2022-02-16 12:58:16 +0200

    Update project files

--- schema.sql
+++ schema.sql
@@ -12,3 +12,26 @@ CREATE TABLE animals (
 /* Alter animals table and add new column*/
 
 ALTER TABLE animals ADD COLUMN species varchar;
+
+/* Create owners table */
+CREATE TABLE owners (
+ id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+ full_name VARCHAR(50),
+ age INT
+);
+
+/* Create species table */
+CREATE TABLE species (
+ id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+ name VARCHAR(50)
+);
+
+/* Modify primary key */
+
+
+-- Alter Animals Table
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD species_id INT;
+ALTER TABLE animals ADD CONSTRAINT fk_name FOREIGN KEY(species_id) REFERENCES species(id);
+ALTER TABLE animals ADD owner_id INT;
+ALTER TABLE animals ADD CONSTRAINT fk_name2 FOREIGN KEY(owner_id) REFERENCES owners(id);
